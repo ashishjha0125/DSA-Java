792398d6a0f0b3a584678e9a6726ec81bd929dcc
ashishjha0125 <88572968+ashishjha0125@users.noreply.github.com>
2025-06-15 14:14:48 +0530

    Selection Sort

---
+++ Sorting (Bubble, Selection, Insertion, Merge, Quick, Heap Sort)/selectionsort.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class selectionsort {
+
+  public static int[] array() { // Method to create an array and return it
+    // This method will create an array of integers and return it
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the size of the array:");
+    int size = s.nextInt();
+    int[] arr = new int[size];
+    System.out.println("Enter the element in Array");
+    for (int i = 0; i < size; i++) {
+      arr[i] = s.nextInt();
+
+    }
+    return arr;
+  }
+
+  public static void printarray(int arr[]) {// Method to print the elements of the array
+    // This method will print the elements of the array passed as an argument
+    int size = arr.length;
+    System.out.println("The elements in the array are:");
+    for (int i = 0; i < size; i++) {
+      System.out.print(arr[i] + " ");
+    }
+
+  }
+
+  // this function is used for selection sort
+  public static int[] selectionsorts(int array[]) {
+
+    int n = array.length;
+
+    for (int i = 0; i <= n - 2; i++) {
+      int minindex = i;
+      for (int j = i; j < n - 1; j++) {
+        if (array[j] < array[minindex]) {
+          minindex = j;
+
+        }
+        int temp = array[minindex];
+        array[minindex] = array[i];
+        array[i] = temp;
+      }
+
+    }
+
+    return array; // Return the sorted array
+
+  }
+
+  public static void main(String[] args) {
+    int array[] = array();// Calling the method to create an array and store it in a variable
+    // Calling the method to print the elements of the array
+    printarray(array);// This will print the elements of the array
+    System.out.println(); // Print a new line for better readability
+
+    int sortedarray[] = selectionsorts(array);
+    printarray(sortedarray); // This will print the sorted elements of the array
+    System.out.println(); // Print a new line for better readability
+
+  }
+}
