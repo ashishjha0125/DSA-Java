707ab3ad77002570edd2f27826526da2ce9a9b3c
ashishjha0125 <88572968+ashishjha0125@users.noreply.github.com>
2025-06-20 16:40:06 +0530

    largest element

--- Array/largestelement.java
+++ Array/largestelement.java
@@ -1,3 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class largestelement {
 
+  public static int brutmethod(int array[], int n) {// timecomplexity O(nlogn) and space complexity O(1)
+    Arrays.sort(array);
+    int largest = array[n - 1];
+
+    return largest;
+  }
+
+  public static int optimisedmethod(int array[], int n) {// timecomplexity O(n) and space complexity O(1)
+    int largest = array[0];
+    for (int i = 0; i < n; i++) {
+      if (array[i] > largest) {
+        largest = array[i];
+      }
+    }
+
+    return largest;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array: ");
+    int size = sc.nextInt(); // Input the size of the array
+    int array[] = new int[size];
+
+    for (int i = 0; i < size; i++) {
+      System.out.println("Enter element " + (i + 1) + ": ");
+      array[i] = sc.nextInt(); // Input each element of the array
+    }
+
+    int largest = brutmethod(array, size); // Calling the method to find the largest element
+    // Output the largest element
+    System.out.println("The largest element in the array using Brut Force is: " + largest);
+
+    int largestOptimised = optimisedmethod(array, size); // Calling the optimised method
+    System.out.println("The largest element in the array using optimised method is: " + largestOptimised);
+
+  }
 }
