c08b4a3ab654da4b26790189a6bcfd9d73b16ec5
Ashish <ashishjha97181@gmail.com>
2025-07-15 20:20:59 +0530

    inheritance

---
+++ OOPS Java/inheritance.java
@@ -0,0 +1,27 @@
+ class Animal {
+  public void sound() {
+    System.out.println("Animal makes a sound");
+  }
+ }
+ class Dog extends Animal {
+ @Override
+  public void sound() {
+    System.out.println("Dog barks");
+  }
+ }
+ class Cat extends Dog {
+ 
+  public void sounde() {
+    System.out.println("Cat meows");
+  }
+ }
+
+
+
+public class inheritance {
+  public static void main(String[] args) {
+    Cat c = new Cat();
+    c.sound();
+    c.sound();
+  }
+}
